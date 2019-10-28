a187e1083eb064d2b90cfb291cfc7c9f76f2b2f8
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-28 00:02:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
+class Dog
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def name 
+    @name
+  end 
+end 
 
 
 
@@ -7,7 +16,15 @@
 
 
 
-
+# class Person
+#   def initialize(name)
+#     @name = name 
+#   end  
+  
+#   def name 
+#     @name = name 
+#   end 
+# end   
 
 
 
